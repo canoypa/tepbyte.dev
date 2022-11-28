843f097849d2a301387960102953bbd2412904df
Cano <developer.sasazono@gmail.com>
2022-11-28 17:47:28 +0900

    Make scroll trigger hook (#3)

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -1,10 +1,21 @@
+import clsx from "clsx";
 import { FC } from "react";
+import { useScrollTrigger } from "~/hooks/scroll_trigger";
 import styles from "./header.module.scss";
 import { MainNavigation } from "./main_navigation";
 
 export const Header: FC = () => {
+  const isFloating = useScrollTrigger({
+    threshold: 0,
+    disableHysteresis: true,
+  });
+
   return (
-    <header className={styles.root}>
+    <header
+      className={clsx(styles.root, {
+        [styles.floating]: isFloating,
+      })}
+    >
       <MainNavigation />
     </header>
   );
