4ab4e9e0917f20d573906d43c51c46d761889d35
Cano <developer.sasazono@gmail.com>
2022-11-29 17:44:06 +0900

    fetcher

---
+++ src/api/profile.ts
@@ -0,0 +1,27 @@
+import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
+import { firebaseAdminApp } from "~/client/firebase-admin";
+
+export const fetchProfile = async () => {
+  const storage = getStorage(firebaseAdminApp);
+
+  const query = storage
+    .bucket("tepbyte.appspot.com")
+    .file("profile/data/_parsed.json");
+  const snapshot = await query.download();
+
+  const data = JSON.parse(snapshot.toString());
+
+  return data;
+};
+
+export const fetchProfileMeta = async () => {
+  const firestore = getFirestore(firebaseAdminApp);
+
+  const query = firestore.doc("profile/data");
+  const snapshot = await query.get();
+
+  const data = snapshot.data();
+
+  return data;
+};
