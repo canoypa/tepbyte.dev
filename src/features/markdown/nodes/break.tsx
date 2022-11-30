ab4a94ecc2dd91a977a8320350d66fe482d199a9
Cano <developer.sasazono@gmail.com>
2022-11-30 22:16:17 +0900

    basic components

---
+++ src/features/markdown/nodes/break.tsx
@@ -0,0 +1,5 @@
+import { MdFC } from "../types";
+
+export const Break: MdFC = () => {
+  return <br />;
+};
--- src/features/markdown/nodes/index.ts
+++ src/features/markdown/nodes/index.ts
@@ -1,6 +1,24 @@
 import { MarkdownComponents } from "../types";
+import { Break } from "./break";
+import { Delete } from "./delete";
+import { Emphasis } from "./emphasis";
+import { Heading } from "./heading";
+import { InlineCode } from "./inline_code";
+import { Link } from "./link";
+import { Paragraph } from "./paragraph";
+import { Root } from "./root";
+import { Strong } from "./strong";
 import { Text } from "./text";
 
 export const components: MarkdownComponents = {
+  root: Root,
+  paragraph: Paragraph,
+  heading: Heading,
   text: Text,
+  emphasis: Emphasis,
+  strong: Strong,
+  inlineCode: InlineCode,
+  delete: Delete,
+  link: Link,
+  break: Break,
 };
