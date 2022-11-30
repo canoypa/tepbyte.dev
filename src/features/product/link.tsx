4503207fb144652ed1891c6e45521602fb1e484f
Cano <developer.sasazono@gmail.com>
2022-11-30 10:31:46 +0900

    product links

--- src/features/product/link.tsx
+++ src/features/product/link.tsx
@@ -1,3 +1,4 @@
+import { default as NextLink } from "next/link";
 import { FC } from "react";
 import { Chip } from "~/components/chip";
 import styles from "./link.module.scss";
@@ -14,10 +15,26 @@ export type LinkProps = {
 export const Link: FC<LinkProps> = ({ links }) => {
   return (
     <div className={styles.root}>
-      {links.homepage && <Chip label="Home Page" />}
-      {links.play_store && <Chip label="Google Play" />}
-      {links.app_store && <Chip label="App Store" />}
-      {links.repository && <Chip label="Repository" />}
+      {links.homepage && (
+        <NextLink href={links.homepage}>
+          <Chip label="Home Page" />
+        </NextLink>
+      )}
+      {links.play_store && (
+        <NextLink href={links.play_store}>
+          <Chip label="Google Play" />
+        </NextLink>
+      )}
+      {links.app_store && (
+        <NextLink href={links.app_store}>
+          <Chip label="App Store" />
+        </NextLink>
+      )}
+      {links.repository && (
+        <NextLink href={links.repository}>
+          <Chip label="Repository" />
+        </NextLink>
+      )}
     </div>
   );
 };
