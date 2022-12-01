48b9986c59680c0e4d87f13140bbc2660d5f9067
Cano <developer.sasazono@gmail.com>
2022-12-01 15:19:35 +0900

    resolve images
    fixme

--- src/features/blog/thumbnail.tsx
+++ src/features/blog/thumbnail.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { resolveImagePath } from "~/core/image_path_resolver";
 import styles from "./thumbnail.module.scss";
 
 export type ThumbnailProps = {
@@ -6,5 +7,11 @@ export type ThumbnailProps = {
 };
 
 export const Thumbnail: FC<ThumbnailProps> = ({ image }) => {
-  return <img src={image} alt="" className={styles.root} />;
+  return (
+    <img
+      src={resolveImagePath("posts", image)}
+      alt=""
+      className={styles.root}
+    />
+  );
 };
