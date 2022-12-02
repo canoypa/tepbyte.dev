003ee2f8c153f605ec021bfa44c2d74de7f690c1
Cano <developer.sasazono@gmail.com>
2022-12-02 22:43:23 +0900

    add favicon

--- src/core/generate_head.tsx
+++ src/core/generate_head.tsx
@@ -17,6 +17,10 @@ export const generateHead = (options: HeadOptions = {}): ReactElement => {
     <>
       <title>{title}</title>
 
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
       <meta name="viewport" content="width=device-width" />
     </>
   );
