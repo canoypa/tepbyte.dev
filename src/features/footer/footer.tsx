55ff2baec85b60b2a4592996bf9a3490476f5d6c
Cano <developer.sasazono@gmail.com>
2022-12-04 13:28:21 +0900

    add footer

---
+++ src/features/footer/footer.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { FC } from "react";
+import { LogoLabel } from "../logo";
+import styles from "./footer.module.scss";
+
+export const Footer: FC = () => {
+  return (
+    <footer className={styles.root}>
+      <LogoLabel height={32} />
+      <Link href="/privacy">Privacy</Link>
+    </footer>
+  );
+};
