b6cacd51bf8afacc47823e6491b2e189d6b480f3
Cano <developer.sasazono@gmail.com>
2022-12-05 16:06:32 +0900

    revalidate

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,19 @@
+import { NextApiHandler } from "next";
+
+const handler: NextApiHandler = async (request, response) => {
+  if (request.method !== "POST") {
+    return response.status(405).end();
+  }
+
+  if (request.body.token !== process.env.REVALIDATE_TOKEN) {
+    return response.status(401).end();
+  }
+
+  try {
+    await response.revalidate(request.body.path);
+    return response.json({ revalidated: true });
+  } catch (err) {
+    return response.status(500).end();
+  }
+};
+export default handler;
