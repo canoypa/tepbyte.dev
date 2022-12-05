66cbcc2d55a0d8cc1519ea39f1f2e7f7e95d9192
Cano <developer.sasazono@gmail.com>
2022-12-05 11:59:35 +0900

    ssg post and products

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,13 @@
-import { fetchPost } from "~/api/post";
+import { fetchPost, fetchPostList } from "~/api/post";
 import { PostInfo, Thumbnail } from "~/features/blog";
 import { MainContents } from "~/features/main_contents";
 import { MarkdownRenderer } from "~/features/markdown";
+
+export const generateStaticParams = async () => {
+  const posts = await fetchPostList();
+
+  return posts.map((post) => ({ slug: post.id }));
+};
 
 const PostPage = async ({ params: { slug } }: { params: { slug: string } }) => {
   const post = await fetchPost(slug);
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,7 +1,13 @@
-import { fetchProduct } from "~/api/product";
+import { fetchProduct, fetchProductList } from "~/api/product";
 import { MainContents } from "~/features/main_contents";
 import { MarkdownRenderer } from "~/features/markdown";
 import { Info, Screenshot } from "~/features/product";
+
+export const generateStaticParams = async () => {
+  const products = await fetchProductList();
+
+  return products.map((product) => ({ slug: product.id }));
+};
 
 const ProductPage = async ({
   params: { slug },
