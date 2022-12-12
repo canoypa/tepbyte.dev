949593a8db14e8f1204bb3403a282c6d94feab57
Cano <developer.sasazono@gmail.com>
2022-12-12 09:07:25 +0900

    add google tag script

--- src/features/client_provider/client_provider.tsx
+++ src/features/client_provider/client_provider.tsx
@@ -1,9 +1,37 @@
 import Script from "next/script";
 import { FC } from "react";
 
+declare global {
+  interface Window {
+    // Google tag
+    dataLayer: Record<string, any>[];
+  }
+}
+
 export const ClientProvider: FC = () => {
   return (
     <>
+      {/* Google tag */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        onLoad={() => {
+          window.dataLayer = window.dataLayer || [];
+
+          function gtag(..._: any[]) {
+            window.dataLayer.push(arguments);
+          }
+
+          gtag("js", new Date());
+
+          if (process.env.NODE_ENV === "production") {
+            gtag("config", process.env.NEXT_PUBLIC_GA_ID);
+          } else {
+            gtag("config", process.env.NEXT_PUBLIC_GA_ID, { debug_mode: true });
+          }
+        }}
+      />
+
       {/* Google AdSense */}
       <Script
         async
