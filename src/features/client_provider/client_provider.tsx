494516b76bc6d58d68df24b4b5426985c017942a
Cano <developer.sasazono@gmail.com>
2022-12-12 09:57:29 +0900

    Remove client firebase (#31)

--- src/features/client_provider/client_provider.tsx
+++ src/features/client_provider/client_provider.tsx
@@ -1,15 +1,37 @@
-import { getAnalytics } from "firebase/analytics";
 import Script from "next/script";
 import { FC } from "react";
-import { firebaseApp } from "~/client/firebase";
 
-if (typeof window !== "undefined") {
-  getAnalytics(firebaseApp);
+declare global {
+  interface Window {
+    // Google tag
+    dataLayer: Record<string, any>[];
+  }
 }
 
 export const ClientProvider: FC = () => {
   return (
     <>
+      {/* Google tag */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        onLoad={() => {
+          window.dataLayer = window.dataLayer || [];
+
+          function gtag(..._: any[]) {
+            window.dataLayer.push(arguments);
+          }
+
+          gtag("js", new Date());
+
+          if (process.env.NODE_ENV === "production") {
+            gtag("config", process.env.NEXT_PUBLIC_GA_ID);
+          } else {
+            gtag("config", process.env.NEXT_PUBLIC_GA_ID, { debug_mode: true });
+          }
+        }}
+      />
+
       {/* Google AdSense */}
       <Script
         async
