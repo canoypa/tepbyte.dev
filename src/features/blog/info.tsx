3d02c08841fdba4932b32106aed88308da56a29b
Cano <developer.sasazono@gmail.com>
2023-01-28 17:12:02 +0900

    date

--- src/features/blog/info.tsx
+++ src/features/blog/info.tsx
@@ -7,18 +7,24 @@ export type PostInfoProps = {
 };
 
 export const PostInfo: FC<PostInfoProps> = ({ post }) => {
+  const dateFormatter = Intl.DateTimeFormat("en-us", { dateStyle: "long" });
+
+  const publishedAt = dateFormatter.format(Date.parse(post.publishedAt));
+  const updatedAt =
+    post.updatedAt && dateFormatter.format(Date.parse(post.updatedAt));
+
   return (
     <div className={styles.root}>
       <div className={styles.primary}>
         <h1 className={styles.title}>{post.title}</h1>
         <p className={styles.subhead}>{post.subhead}</p>
       </div>
-      <div className={styles.date}>
-        <span>{post.publishedAt}</span>
-        {post.updatedAt && (
+      <div>
+        <time dateTime={post.publishedAt}>{publishedAt}</time>
+        {updatedAt && (
           <>
-            <span>-</span>
-            <span>Updated {post.updatedAt}</span>
+            <span> - </span>
+            <time dateTime={post.updatedAt}>Updated {updatedAt}</time>
           </>
         )}
       </div>
