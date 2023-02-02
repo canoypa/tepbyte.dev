f4ba4d73eb10b62f03ec957acf277567eaa649e5
Cano <developer.sasazono@gmail.com>
2023-02-02 20:07:40 +0900

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,23 @@ const lineClampPlugin = require("@tailwindcss/line-clamp");
 
 const m3Plugin = require("tailwindcss-material3-plugin").Material3Plugin({
   sourceColor: 0x8282F4,
+  customColors: [
+    {
+      name: "info",
+      value: 0x42a5f5,
+      blend: true,
+    },
+    {
+      name: "warning",
+      value: 0xffee58,
+      blend: true,
+    },
+    {
+      name: "success",
+      value: 0x66bb6a,
+      blend: true,
+    }
+  ]
 });
 
 /** @type {import('tailwindcss').Config} */
