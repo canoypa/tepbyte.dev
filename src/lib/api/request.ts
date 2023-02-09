270b43e97100099a44f3718c141fdc9a06abe9fe
Cano <developer.sasazono@gmail.com>
2023-02-09 19:33:23 +0900

    fix

--- src/lib/api/request.ts
+++ src/lib/api/request.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
-import { JWT } from "google-auth-library";
+import axios from 'axios';
+import { JWT } from 'google-auth-library';
 
 const client = new JWT({
   email: process.env.CLIENT_EMAIL,
-  key: process.env.PRIVATE_KEY,
+  key: process.env.PRIVATE_KEY?.split(String.raw`\n`).join('\n'),
 });
 
-const API_ORIGIN = "https://tepbyte-qy5wbcvsoq-an.a.run.app";
+const API_ORIGIN = 'https://tepbyte-qy5wbcvsoq-an.a.run.app';
 
 export const request = async <T = unknown>(path: string, params?: any) => {
   const url = new URL(path, API_ORIGIN).toString();
