fc2e30689e2c07b11b38ef2e0bcb8f84cf88bbf4
Cano <developer.sasazono@gmail.com>
2023-06-14 10:41:10 +0900

    remove shadow

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -8,7 +8,7 @@ import { MainNavigation } from './main_navigation';
 const styles = {
   root: /* Tailwind */ tw`
     group/header sticky top-0 bg-dark-surface/70 backdrop-blur-lg
-    data-[floating=true]:shadow-2 z-2`,
+    data-[floating=true]:z-2`,
   container: /* Tailwind */ tw`
     flex justify-center duration-short-2 ease-standard transition
     group-data-[floating=true]/header:bg-dark-primary/2`,
