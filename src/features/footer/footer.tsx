c9ea4b34c771cb3778fdb7b52083a6f9ad5b37fd
Cano <developer.sasazono@gmail.com>
2023-06-14 10:21:06 +0900

    Remove logo and privacy page (#66)

--- src/features/footer/footer.tsx
+++ src/features/footer/footer.tsx
@@ -1,19 +1,20 @@
-import Link from 'next/link';
 import { FC } from 'react';
+import { StyledLink } from '~/components/styled_link';
 import { tw } from '~/lib/tw';
-import { LogoLabel } from '../logo';
 
 const styles = {
-  root: /* Tailwind */ tw`
-    flex items-center gap-x-4 px-6 py-8 text-dark-on-surface-variant fill-dark-on-surface-variant font-comfortaa text-body-medium
-    md:gap-x-8 md:px-16 md:py-12`,
+  root: /* Tailwind */ tw`mx-4 px-2 py-6 border-t-[1px] border-t-dark-outline-variant text-dark-on-surface-variant text-body-small`,
 };
 
 export const Footer: FC = () => {
   return (
     <footer className={styles.root}>
-      <LogoLabel height={32} />
-      <Link href="/privacy">Privacy</Link>
+      このサイトは Google Analytics を使用しています。詳しくは{' '}
+      <StyledLink href="https://policies.google.com/technologies/partner-sites">
+        Google のサービスを使用するサイトやアプリから収集した情報の Google
+        による使用
+      </StyledLink>{' '}
+      をご覧ください。
     </footer>
   );
 };
