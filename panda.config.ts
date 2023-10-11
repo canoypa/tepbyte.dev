7caea2cddcb24950af47caa33f0c2cc5ec770f87
Cano <developer.sasazono@gmail.com>
2023-10-11 22:41:34 +0900

    add pandacss-preset-material-tokens

--- panda.config.ts
+++ panda.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from '@pandacss/dev'
+import { presetMaterialTokens } from 'pandacss-preset-material-tokens'
 
 export default defineConfig({
   // The output directory for your css system
@@ -14,4 +15,15 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {},
+
+  presets: [
+    presetMaterialTokens({
+      sourceColor: 0x8282f4,
+      customColors: [
+        { name: 'info', value: 0x42a5f5, blend: true },
+        { name: 'warning', value: 0xffee58, blend: true },
+        { name: 'success', value: 0x66bb6a, blend: true },
+      ],
+    }),
+  ],
 })
