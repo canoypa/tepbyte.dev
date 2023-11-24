4712974a39ba1dba12b095460964f38da0997812
Cano <developer.sasazono@gmail.com>
2023-11-24 16:43:00 +0900

    using fetch api (#90)

--- src/lib/api/posts.ts
+++ src/lib/api/posts.ts
@@ -1,16 +1,16 @@
 import { cache } from 'react'
-import { request } from '~/lib/api/request'
+import { requestAPI } from '~/lib/api/request'
 import { ArticleMeta } from '~/types/parsed'
 
 type Article = { meta: ArticleMeta; body: any }
 export const get = cache(async (params: { slug: string }) => {
   const path = 'posts/get'
-  const data = await request<Article>(path, params)
+  const data = await requestAPI<Article>(path, params)
   return data
 })
 
 export const list = cache(async (params?: { limit?: number }) => {
   const path = 'posts/list'
-  const data = await request<ArticleMeta[]>(path, params)
+  const data = await requestAPI<ArticleMeta[]>(path, params)
   return data
 })
