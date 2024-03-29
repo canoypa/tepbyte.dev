5e185cdbe8bfd221c628a9238bb551187e048c02
Cano <developer.sasazono@gmail.com>
2024-03-29 16:34:55 +0900

    update hero

--- src/features/profile/hero.tsx
+++ src/features/profile/hero.tsx
@@ -1,24 +1,42 @@
 import { FC } from 'react'
+import { Image } from '~/components/image'
 import { ProfileMeta } from '~/types/parsed'
 import { css } from '~pandacss/css'
 import { flex } from '~pandacss/patterns'
-import { ProfileCard } from './card'
 import { ProfileLink } from './profile_link'
 
 const styles = {
   root: css({
-    display: 'flex',
-    flexDirection: { base: 'column', md: 'row' },
+    display: 'grid',
+    gridTemplate: 'auto / auto 1fr',
+    gridTemplateAreas: {
+      base: '"photo name" "info info"',
+      md: '"photo name" "photo info"',
+    },
     alignItems: 'center',
-    gap: 64,
-    paddingX: 32,
-    paddingY: 64,
-  }),
-  card: flex({ justifyContent: 'center' }),
-  info: flex({ direction: 'column', gap: 32 }),
-  subhead: css({
-    textStyle: { base: 'headline-small', md: 'headline-medium' },
+    columnGap: { base: 24, md: 32 },
+    rowGap: 16,
+
     fontFamily: 'comfortaa',
+  }),
+  photo: css({
+    gridArea: 'photo',
+
+    width: { base: 96, md: 128 },
+    height: { base: 96, md: 128 },
+    rounded: 'full',
+  }),
+  name: css({
+    gridArea: 'name',
+
+    textStyle: 'display-small',
+  }),
+  info: flex({
+    gridArea: 'info',
+
+    direction: 'column',
+    rowGap: 16,
+    textStyle: 'body-large',
   }),
 }
 
@@ -29,15 +47,18 @@ export type HeroProps = {
 export const Hero: FC<HeroProps> = ({ profile }) => {
   return (
     <div className={styles.root}>
-      <div className={styles.card}>
-        <ProfileCard
-          name={profile.name}
-          photo={profile.photo}
-          image={profile.background}
-        />
-      </div>
+      <Image
+        src={profile.photo.url}
+        alt=""
+        width={profile.photo.width}
+        height={profile.photo.height}
+        className={styles.photo}
+        blurDataUrl={profile.photo.blurDataUrl}
+        priority
+      />
+      <span className={styles.name}>{profile.name}</span>
       <div className={styles.info}>
-        <span className={styles.subhead}>{profile.subhead}</span>
+        <span>{profile.subhead}</span>
         <ProfileLink links={profile.links} />
       </div>
     </div>
--- src/features/profile/index.ts
+++ src/features/profile/index.ts
@@ -1,6 +1,2 @@
-export { Avatar } from './avatar'
-export type { AvatarProps } from './avatar'
-export { ProfileCard } from './card'
-export type { ProfileCardProps } from './card'
-export { Hero } from './hero'
-export type { HeroProps } from './hero'
+export { Hero } from "./hero";
+export type { HeroProps } from "./hero";
