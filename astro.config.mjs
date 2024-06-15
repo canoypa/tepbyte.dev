5a4ec466c2f5c5807d429f004bf7361176c98f0d
Cano <41741040+canoypa@users.noreply.github.com>
2024-06-16 00:45:42 +0900

    markdown config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,24 @@ import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
 
+import remarkBreaks from 'remark-breaks'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
+  markdown: {
+    remarkPlugins: [remarkBreaks],
+
+    remarkRehype: {
+      footnoteLabelProperties: {
+        ariaHidden: true,
+        hidden: true,
+      },
+    },
+
+    shikiConfig: {
+      theme: 'github-dark-default',
+    },
+  },
 })
