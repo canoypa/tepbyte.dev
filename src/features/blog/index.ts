a330442438382ac41e8fa99b37065b5646047b53
Cano <41741040+canoypa@users.noreply.github.com>
2024-07-06 00:41:12 +0900

    静的なコンポーネントを全てastroに移行 (#119)
    * button
    
    * blog list
    
    * product list
    
    * blog info
    
    * product info
    
    * product link
    
    * profile links
    
    * footer
    
    * main contents
    
    * styled link
    
    * page headline
    
    * tags
    
    * tag icon
    
    * chip

--- src/features/blog/index.ts
+++
@@ -1,4 +0,0 @@
-export { PostInfo } from './info'
-export type { PostInfoProps } from './info'
-export { BlogList } from './list'
-export type { BlogListProps } from './list'
--- src/features/product/index.ts
+++
@@ -1,4 +0,0 @@
-export { Info } from './info'
-export type { InfoProps } from './info'
-export { ProductList } from './list'
-export type { ProductListProps } from './list'
