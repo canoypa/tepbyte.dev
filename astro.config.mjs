e2bb2617d9e465f788a11b2fe229227c115d37a4
Cano <41741040+canoypa@users.noreply.github.com>
2024-10-06 20:14:45 +0900

    never trailingSlash

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import remarkBreaks from 'remark-breaks'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.tepbyte.dev',
+  trailingSlash: 'never',
 
   integrations: [solidJs(), compress()],
 
